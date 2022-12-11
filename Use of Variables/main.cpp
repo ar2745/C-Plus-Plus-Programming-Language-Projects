c56070774726897cecc165b4c131aadb427cf96f
ar2745 <76749119+ar2745@users.noreply.github.com>
2022-12-10 23:26:49 -0500

    Add files via upload

---
+++ Use of Variables/main.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <string>
+
+/*The author of this code is Andrew Robinson
+    Date:12/24/21
+    This is for education
+    GitHub ID
+
+    Data Types
+
+    char
+    int
+    short int
+    long int
+    float
+    double
+
+    %s = string ""
+    %d = integer
+    %f = float
+
+    Arithmetic Operators
+
+    Addition +
+    Subtraction -
+    Division /
+    Modulus %
+    Increment ++
+    Decrement --
+
+    Relational Operators
+
+    Equivalent ==
+    Not Equal !=
+    Greater Than >
+    Less Than <
+    Greater Than or Equal >=
+    Less Than or Equal <=
+
+
+    if(x < y)
+
+        cout << "x is less than y" << endl;
+
+    else
+
+        cout << "x is not less than y" << endl;
+
+
+    Logical Operators
+
+    And Operator &&
+    Or Operator ||
+    Not Operator !
+
+    String Functions
+
+    strcpy
+    strcat
+    strlen
+    strcmp
+    strchr
+    strstr
+
+    Escape Sequences C++ Programming Language
+
+    New Line \n
+    Form Feed \f
+    Backspace \b
+    Horizontal Tap \t
+    Carriage Return \r
+    Vertical Tap \v
+    Character \' or \"
+    Alert \a
+
+    Bitwise Operators
+
+    Binary &
+    Or |
+    Exal ^
+    Shift Left <<
+    Shift Right >>
+    */
+
+using namespace std;
+
+int main()
+{
+    int x, y, z;
+    string hello = "Andrew Robinson";
+
+    x = 20;
+    y = 9;
+    z = x + y;
+
+    cout << "Use of Variable in C++" << endl;
+    cout << "The result of x + y = " << z << endl;
+    cout << "My name is " << hello << endl;
+
+    return 0;
+}
